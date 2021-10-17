d5cdb90521a30e599687e4da4983a434ccea7ca4
bchadwic <bchadwick@mail.greenriver.edu>
2021-10-17 14:59:01 -0700

    worked with variables and operators

---
+++ Start/Overview/Operators/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declare some variables to excercise the operators
+            int x=10, y=5;
+            string a="abcd", b="efgh";
+
+            // TODO: Basic math operators are +, -, /, *
+            //Console.WriteLine("----- Basic Math -----");
+            //Console.WriteLine((x/y) * x);
+            //Console.WriteLine(a + b);
+
+
+
+            // TODO: Increment / decrement operators
+            //Console.WriteLine("----- Shorthand -----");
+            //x++;
+            //y++;
+            //Console.WriteLine(x);
+            //Console.WriteLine(y);
+
+
+            // TODO: Operators can be shorthand: a = a + b
+            //a += b;
+            //Console.WriteLine(a);
+
+            // TODO: Logical operators &&, ||
+            // Console.WriteLine("----- Logic Operators -----");
+            //Console.WriteLine(x > y && y >= 5);
+            //Console.WriteLine(x > y || y >= 5);
+
+
+            // null-coalescing operators
+            string str = null;
+            Console.WriteLine(str ?? "Unknown String"); 
+            
+            str ??= "New String";
+            Console.WriteLine(str ?? "Unknown String"); 
+        }
+    }
+}
