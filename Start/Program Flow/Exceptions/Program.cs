ed78917b7c3d9b72947c34885cdb4ab5a32840e9
bchadwic <bchadwick@mail.greenriver.edu>
2021-10-17 15:36:15 -0700

    added in a try catch statement

---
+++ Start/Program Flow/Exceptions/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Exceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 100;
+            int y = 0;
+            int result;
+
+            try {
+                result = x / y;
+            } catch (DivideByZeroException e){
+                Console.WriteLine(e.Message);
+            } catch (ArgumentOutOfRangeException e) {
+                Console.WriteLine("x has to be 1000 or less");
+            } finally {
+                Console.WriteLine("this always runs");
+            }
+            Console.WriteLine("The result is: {0}", result);
+        }
+    }
+}
